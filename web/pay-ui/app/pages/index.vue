4f029cf8781130b687a95130dadcb3c2f7736442
deetz99 <dietrich@wolpert.ca>
2025-09-19 14:57:07 -0700

    set package version to 2.0.0-beta

--- web/pay-ui/app/pages/index.vue
+++ web/pay-ui/app/pages/index.vue
@@ -19,25 +19,27 @@ setBreadcrumbs([
 <template>
   <div class="py-10 space-y-10">
     <h1>{{ $t('page.home.h1') }}</h1>
-    <UButton
-      label="Go to Protected"
-      :to="localePath('/protected')"
-    />
-    <UButton
-      label="Open example modal"
-      @click="modal.openExampleModal(
-        'Title Here',
-        'Description Text',
-        false,
-        [
-          { label: 'Close', shouldClose: true },
-          { label: 'Alert',
-            onClick: () => {
-              useToast().add({ title: 'Toast Title' })
+    <div class="flex gap-10">
+      <UButton
+        label="Go to Protected"
+        :to="localePath('/protected')"
+      />
+      <UButton
+        label="Open example modal"
+        @click="modal.openExampleModal(
+          'Title Here',
+          'Description Text',
+          false,
+          [
+            { label: 'Close', shouldClose: true },
+            { label: 'Alert',
+              onClick: () => {
+                useToast().add({ title: 'Toast Title' })
+              }
             }
-          }
-        ]
-      )"
-    />
+          ]
+        )"
+      />
+    </div>
   </div>
 </template>
